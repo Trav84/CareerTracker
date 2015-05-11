45188d0d31e740c4a51b5b040cd221a9998d0e09
Travis Czerw <travis.czerw@gmail.com>
2015-05-11 18:42:20 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// var ref = new Firebase('https://blistering-torch-743.firebaseio.com/');
+
+// var usersRef = ref.child("profiles");
+// usersRef.set({
+// 		travisczerw: {
+// 			applications: 5,
+// 			referrals: 7,
+// 			interviews: 7,
+// 			technicalInterviews: 5
+// 	},
+// });
+
+// // Attach an asynchronous callback to read the data at our posts reference
+// ref.on("value", function(snapshot) {
+// 	console.log(snapshot.val());
+// }, function (errorObject) {
+// 	console.log("The read failed: " + errorObject.code);
+// });
